7412a1b092a2e5ee9310bdd5c52bc750de07d6ee
4FViP <152526348+4FViP@users.noreply.github.com>
2024-02-07 16:19:22 +0200

    Add files via upload

---
+++ group_add_members.py
@@ -0,0 +1,36 @@
+from pyrogram import Client
+# DASTURCHI @ALAWE1 Kanal : @AWCODE3 "تخمط تثبت فشلك 
+"""
+PYROGRAM Module orqalik odiygina tayorlangan kod...
+Shunchaki bu kod orqalik guruhdan gurhga odam olib otishingiz mumkin...
+Tushunarli bo'lishi uchun boshlanishiga odiygina yozilgan...
+Ogohlantirish ! Akuntingiz Spam bo'lsa yoki blok bo'lsa dasturchi javob bermaydi...
+
+Negativ va Senior dasturchilar negativ yozmelar boshlanishiga kod bu...
+
+Manba bilan olinglar do'stlar...
+
+DASTURCHI @ALAWE1 Kanal : @AWCODE3
+
+"""
+api_id =  11750778
+api_hash = 'd0352df3ddb5e00bcf16b55dae071b52' # API HASH KIRITING 
+app = Client(
+    "my_bot",
+    api_id=api_id,
+    api_hash=api_hash
+)
+
+app.start()
+# DASTURCHI @ALAWE1 Kanal : @AWCODE3
+while True:
+    from_chat_id = -1001465512263 # A'zo tanlamoqchi bolgan guruhingiz ID raqamini kiritng
+    my_chat_id = -1001988031774 # A'zolarni qo'shmoqchi bo'lgan guruhingiz ID raqamini kiritng
+    members = [] # Foidalanuvchi idlarini ro'yxatga ko'chirish yani olish uchun
+    # DASTURCHI @ALAWE1 Kanal : @AWCODE3
+    for member in app.get_chat_members(from_chat_id):
+        members.append(member.user.id)
+    # DASTURCHI @ALAWE1 Kanal : @AWCODE3
+    app.add_chat_members(chat_id=my_chat_id, user_ids=members)
+# DASTURCHI @ALAWE1 Kanal : @AWCODE3
+app.run()
